eada4a7b5c94cee25b32a6c30920a12ed66658f5
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2022-02-08 09:15:05 +0530

    Added Intro Video

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
+import ReactPlayer from "react-player/lazy";
 
 export default function Home() {
   return (
@@ -32,6 +33,50 @@ export default function Home() {
         >
           Hello, I am Goutham Ananda
         </Typography>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          height: "50vh",
+        }}
+        spacing={1}
+      >
+        <Grid
+          xs="12"
+          md="5"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mx: 2,
+          }}
+          order={{ xs: 2, md: 1 }}
+          item
+        >
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iste
+            eius perspiciatis pariatur fugit labore placeat nam. Impedit omnis
+            ut, nihil atque nobis, qui accusantium asperiores placeat quis
+            reprehenderit quasi.
+          </Typography>
+        </Grid>
+        <Grid
+          xs="12"
+          md="6"
+          order={{ xs: 1, md: 2 }}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          item
+        >
+          <ReactPlayer
+            url="https://www.youtube.com/embed/IlpnJe__zJw"
+            width="100%"
+            playing
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
